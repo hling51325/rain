ad85177159d94a5e9427ca9d1133c429c4814ce5
其妙 <hling51325@163.com>
2018-11-09 18:07:03 +0800

    fix

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: "tokine",
-            script: "./run.js",
+            script: "./bin/run.js",
             watch: true,
             env: {
                 "NODE_ENV": "development"
