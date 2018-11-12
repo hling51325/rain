389591a94e829c12d253ca1bb5b1bd8d5273cf0f
其妙 <hling51325@163.com>
2018-11-13 00:41:28 +0800

    add chatroom

--- src/lib/service/webSocket.js
+++ src/lib/service/webSocket.js
@@ -6,6 +6,8 @@ module.exports = {
     init
 };
 
+let users = []
+
 function init(server) {
     if (!server) return;
 
@@ -17,16 +19,28 @@ function init(server) {
     io.on('connection', socket => {
         console.log('a user connect...');
 
-        socket.on('chat message', function (msg) {
-            io.emit('chat message', msg);
+        socket.on('chat-message', function (msg) {
+            io.emit('chat-message', msg);
         });
 
+        socket.on('user-connected', user => {
+            const { username } = user
+            let data = {
+                socketId: socket.id,
+                ...user
+            }
+            users.push(data)
+            socket.broadcast.emit('user-connected', `${username} 加入了聊天室。`);
+            socket.emit('users', users);
+            socket.broadcast.emit('users', users);
+        })
+
         socket.on('disconnect', () => {
-            console.log('a user disconnect...');
+            users = users.filter(user => user.socketId !== socket.id)
         });
         socket.broadcast.emit('hi'); // except current socket
         socket.emit('notification', {}); // send to one
     });
-    
+
     io.emit('something', {}); // send to all
 }
