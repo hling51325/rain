670f0a3bd2e5816649a7c7d74a7e04b58bbcd191
其妙 <hling51325@163.com>
2019-09-20 10:00:25 +0800

    prof

--- src/lib/routes/touch.js
+++ src/lib/routes/touch.js
@@ -25,7 +25,17 @@ module.exports = [
         verb: 'get',
         url: '/mongo-socket-2',
         methods: [mongoTest2]
-    }
+    },
+    // {
+    //     verb: 'get',
+    //     url: '/cpu',
+    //     methods: [cpu]
+    // },
+    // {
+    //     verb: 'get',
+    //     url: '/memory',
+    //     methods: [memory]
+    // }
 ]
 
 const { Test } = require('../schema')
@@ -57,4 +67,23 @@ async function mongoTest2(ctx, next) {
         Test.findOne({ $where: 'sleep(10000) || true' })
     ])
     ctx.response.body = 'ok'
+}
+
+function cpu(ctx) {
+    let count = 0
+    for (let i = 0; i < 5000000000; i++) {
+        count++
+    }
+    ctx.response.body = count
+}
+
+const { timeout } = require('../service/util')
+async function memory(ctx) {
+    let array = []
+    for (let i = 0; i < 10000000; i++) {
+        // array = [...array, i]
+        array.push(i)
+    }
+    await timeout(5000)
+    ctx.body = array
 }
